49cc03372fa23cfa62a138c933ba56cad91223cf
prettymariapurba <prettymaria110406@gmail.com>
2024-05-11 12:11:16 +0700

    21

--- libs/gender.c
+++ libs/gender.c
@@ -1,7 +1,20 @@
 #include "gender.h"
+#include "string.h"
+#include "stdlib.h"
 
 /**
  * @brief Define the complete function definition here. Be sure to enlist the prototype of each function
  * defined here in the corresponding header file.
  *
  */
+
+ char *gender_to_text (enum gender_t mhs_gender){
+    char *data = malloc (10);
+    if (mhs_gender == 0) strcpy (data, "male");
+    else if (mhs_gender == 1) strcpy (data, "female");
+    return data;
+
+ }
+
+
+
